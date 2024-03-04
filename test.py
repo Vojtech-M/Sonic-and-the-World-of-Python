8f2a6c9fbd7c85805fd1d4ff485530b2fb541ccc
Vojtěch Michal <vojtamichal04@gmail.com>
2024-03-04 21:34:52 +0100

    Testing commit from pyCharm

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import pyglet
 
-window = pyglet.window.Window()
+window = pyglet.window.Window( fullscreen = True)
 
-label = pyglet.text.Label('Helloo, Pythonn !',
-                          font_name='Times New Roman',
-                          font_size=44,
+label = pyglet.text.Label('HELLO PYTHON !',
+                          font_name='Lilita One',
+                          font_size=72,
                           x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
-
+                          anchor_x='center', anchor_y='center',
+                         )
+#image = pyglet.resource.image('papousek.jpeg')
 @window.event
 def on_draw():
     window.clear()
+
     label.draw()
+
+   # image.blit(0, 0)
 
 
 pyglet.app.run()
+
+
+window.set_fullscreen(False)
